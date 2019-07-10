d4a4d5a6679c60fc170ef7facbee13c589f594c1
Michael <commentongithub>
2019-07-10 22:58:41 +0200

    example Password file

---
+++ passwdDefault.h
@@ -0,0 +1,10 @@
+/************ WIFI and MQTT Information (CHANGE THESE FOR YOUR SETUP) ******************/
+#define SENSORNAME "porch" //change this to whatever you want to call your device
+
+const char* ssid = "xxx"; //type your WIFI information inside the quotes
+const char* password = "xxx";
+
+const char* mqtt_server = "xxx.xxx.xxx.xxx"; //url / ipaddress
+const char* mqtt_username = "xxx";
+const char* mqtt_password = "xxx";
+const int mqtt_port = xxx;
