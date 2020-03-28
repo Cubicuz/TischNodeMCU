8efc82e5df0da89d197e9256efea1eb40e04878a
michi <a@a>
2020-03-28 23:33:55 +0100

    playing with mqtt discovery

--- include/passwdDefault.h
+++ include/passwdDefault.h
@@ -1,10 +1,10 @@
 /************ WIFI and MQTT Information (CHANGE THESE FOR YOUR SETUP) ******************/
-#define SENSORNAME "porch" //change this to whatever you want to call your device
+const char* DEVICENAME = "TischESP"; //change this to whatever you want to call your device
 
-const char* ssid = "xxx"; //type your WIFI information inside the quotes
-const char* password = "xxx";
+const char* WIFI_SSID = "xxx"; //type your WIFI information inside the quotes
+const char* WIFI_PASSWORD = "xxx";
 
-const char* mqtt_server = "xxx.xxx.xxx.xxx"; //url / ipaddress
-const char* mqtt_username = "xxx";
-const char* mqtt_password = "xxx";
-const int mqtt_port = xxx;
+const char* MQTT_ADDRESS = "xxx.xxx.xxx.xxx";
+const char* MQTT_USERNAME = "xxx";
+const char* MQTT_PASSWORD = "xxx";
+const int MQTT_PORT = 1883; // default 1883
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,84 @@
 #include <Arduino.h>
+#include <MQTT.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+#include "animations.h"
+#include "passwd.h"
+
+MQTTClient homeassistant(300);
+WiFiClient netRef;
+
+void connectWifi(){
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.print("\n");
+}
+
+void initHomeassistant(){
+  homeassistant.begin(MQTT_ADDRESS, netRef);
+}
+
+void connectHomeassistant(){
+  Serial.print("connect mqtt...");
+  while (!homeassistant.connect(DEVICENAME, MQTT_USERNAME, MQTT_PASSWORD)){
+    Serial.print(".");
+    delay(1000);
+  }
+  homeassistant.publish("test", "test");
+  Serial.print("\n");
+}
+
+void registerLight(){
+  DynamicJsonDocument doc(256);
+  doc["~"] = "homeassistant/light/table";
+  doc["name"] = "Tisch";
+  doc["unique_id"] = "table_light";
+  doc["cmd_t"] = "~/set";
+  doc["stat_t"] = "~/state";
+  doc["schema"] = "json";
+  doc["brightness"] = true;
+  doc["rgb"] = true;
+  doc["white_value"] = true;
+  doc["optimistic"] = true;
+  doc["effect"] = true;
+  JsonArray effectList = doc.createNestedArray("effect_list");
+  effectList.add("rainbow");
+  effectList.add("rainbowmove");
+  effectList.add("raining");
+  
+  Serial.print("doc Memoryusage: ");
+  Serial.print(doc.memoryUsage());
+  Serial.println();
+  char output[256];
+  Serial.print("mqtt memory: ");
+  Serial.print(serializeJson(doc, output));
+  Serial.print(" ");
+  Serial.println(strlen(output));
+  Serial.println(output);
+  Serial.print("publish: ");
+  Serial.println(homeassistant.publish("homeassistant/light/table/config", output));
+}
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(19200);
+  Serial.println();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  homeassistant.begin(MQTT_ADDRESS, MQTT_PORT, netRef);
+  connectWifi();
+  connectHomeassistant();
+  registerLight();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (!homeassistant.connected()){
+    connectWifi();
+    connectHomeassistant();
+  }
+  homeassistant.loop();
 }
