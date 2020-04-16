e48e477042fffe404bba1b2b0595884c1275451d
michi <a@a>
2020-04-16 21:15:19 +0200

    new stripwrapper

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 #include <MQTT.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
+#define NUM_LEDS 30
+#define DATA_PIN 0 
 
-#include "animations.h"
 #include "passwd.h"
+
+#include "stripwrapper.h"
+
+#define DEBUG
 
 MQTTClient homeassistant(300);
 WiFiClient netRef;
+Adafruit_NeoPixel strip(NUM_LEDS, DATA_PIN, NEO_GRBW + NEO_KHZ800);
+StripWrapper tischleds(&strip);
 
 void connectWifi(){
   Serial.print("checking wifi...");
@@ -49,18 +57,56 @@ void registerLight(){
   effectList.add("rainbow");
   effectList.add("rainbowmove");
   effectList.add("raining");
-  
+
+  const uint outputSize = 256;
+  char output[outputSize];
+  if (serializeJson(doc, output) > outputSize){
+    Serial.println("ARRAY OUT OF BOUNDS");
+    return; 
+  }
+
+#ifdef DEBUG
   Serial.print("doc Memoryusage: ");
   Serial.print(doc.memoryUsage());
   Serial.println();
-  char output[256];
   Serial.print("mqtt memory: ");
-  Serial.print(serializeJson(doc, output));
-  Serial.print(" ");
   Serial.println(strlen(output));
   Serial.println(output);
+#endif
+
+  bool retVal = homeassistant.publish("homeassistant/light/table/config", output);
+#ifdef DEBUG
   Serial.print("publish: ");
-  Serial.println(homeassistant.publish("homeassistant/light/table/config", output));
+  Serial.println(retVal);
+#endif
+}
+
+void mqttCallback(MQTTClient *client, char topic[], char payload[], int payload_length){
+  Serial.println("callback");
+  DynamicJsonDocument doc(128);
+  DeserializationError e = deserializeJson(doc, payload, payload_length);
+  if (e != DeserializationError::Ok){
+    Serial.print(e.c_str());
+  }
+  if (strcmp(doc["state"], "ON") == 0){
+    if (doc.containsKey("color")){
+      Serial.println("color:");
+      Serial.print("r: ");
+      byte red = doc["color"]["r"];
+      byte green = doc["color"]["g"];
+      byte blue = doc["color"]["b"];
+      Serial.println(red);
+    }
+    if (doc.containsKey("white_value")){
+      
+    }
+    if (doc.containsKey("brightness")){
+
+    }
+    if (doc.containsKey("effect")){
+
+    }
+  }
 }
 
 void setup() {
@@ -72,6 +118,12 @@ void setup() {
   connectWifi();
   connectHomeassistant();
   registerLight();
+  homeassistant.onMessageAdvanced(mqttCallback);
+  homeassistant.subscribe("homeassistant/light/table/set");
+  strip.begin();
+  tischleds.setAnimation(tischleds.RAINBOWMOVING);
+  strip.setPixelColor(2, 255, 0, 0);
+  strip.show();
 }
 
 void loop() {
@@ -80,5 +132,7 @@ void loop() {
     connectWifi();
     connectHomeassistant();
   }
+  delay(100);
+  tischleds.animate();
   homeassistant.loop();
 }
