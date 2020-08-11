dd895126d38e7783f014d8b1578ed52894e41748
cubicuz <no@mail.me>
2020-08-11 20:46:40 +0200

    add status received handler, it may work now

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <Adafruit_NeoPixel.h>
 #include "Homeassistant.h"
 #define NUM_LEDS 30
-#define DATA_PIN 0
+#define DATA_PIN D3
+#define MOSFET_PIN D1
 
 #include "StripWrapper.h"
 
@@ -12,15 +13,33 @@ Adafruit_NeoPixel strip(NUM_LEDS, DATA_PIN, NEO_GRBW + NEO_KHZ800);
 StripWrapper tischleds(&strip);
 Homeassistant hs(&tischleds);
 
+bool stripOn;
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(19200);
   Serial.println();
+  pinMode(MOSFET_PIN, OUTPUT);
   strip.begin();
   hs.begin();
   hs.connect();
+  hs.setStatusReceivedCallback([&](const Status &s){
+    if (s.on){
+      if (!stripOn){
+        stripOn = true;
+        digitalWrite(MOSFET_PIN, HIGH);
+      }
+      tischleds.setAnimation(s.animation);
+      tischleds.setBrightness(s.brightness);
+      tischleds.setColor(s.color.channels.r, s.color.channels.g, s.color.channels.b, s.color.channels.w);
+    } else {
+      if (stripOn){
+        stripOn = false;
+        digitalWrite(MOSFET_PIN, LOW);
+      }
+    }
+  });
 }
 
 void loop()
